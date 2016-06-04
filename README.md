354b2b480fabb9449321c51d4f6479be29dbaaf1
Juan Timana <sietbukuel@gmail.com>
2016-06-04 05:59:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Oktober Foundation
+Starter development template for building [Zurb Foundation 6](http://foundation.zurb.com/sites/docs/) themed [October CMS](//octobercms.com/) sites.
+
+This theme was initially based on [f5fresh](https://github.com/Eoler/oc-f5fresh-theme), the main reason I made this theme was because f5fresh is based on Foundation 5 and I needed Foundation 6 (and waiting was not an option).
+
+Includes:
+- Latest Foundation Sites 6 [source from official repository](https://github.com/zurb/foundation-sites)
+- Latest FontAwesome Icons
+- Motion UI
+- Gulp-based build system for development and production
+
+
+## Installation
+Add the theme to existing project while logged into OctoberCMS account online,
+or interactively by searching it inside Settings/System/Updates/Themes in backend,
+or with the following command-line instructions inside *project root*:
+```
+php artisan theme:install zietbukuel.oktober-foundation
+php artisan theme:use oktober-foundation
+```
+
+## One-time Development Environment Setup
+Install [NodeJS and Node Package Manager](https://nodejs.org/en/) globally.
+Run the following command-line instructions inside *theme root*:
+```
+npm install -g gulp bower
+npm install && bower install
+```
+
+## Using and Editing
+Custom styles should be in [assets/scss/app.scss](https://github.com/zietbukuel/oktober-foundation/blob/master/assets/scss/app.scss),
+scripts in [assets/es6/app.js](https://github.com/zietbukuel/oktober-foundation/blob/master/assets/es6/app.js).
+Development changes can be automated with command-line instruction inside *theme root*:
+```
+gulp watch
+```
+Build optimized, minified, autoprefixed assets with command-line instruction inside *theme root*:
+```
+gulp --production
+```
+
+## Customizing Foundation
+Global Foundation styling parameters should be modified in
+[assets/scss/_settings.scss](https://github.com/zietbukuel/oktober-foundation/blob/master/assets/scss/_settings.scss)
+Comment out unwanted styles and scripts for leaner and faster Foundation custom build:
+[assets/scss/_foundation6.scss](https://github.com/zietbukuel/oktober-foundation/blob/master/assets/scss/_foundation6.scss)
+[assets/es6/app.js](https://github.com/zietbukuel/oktober-foundation/blob/master/assets/es6/app.js)
+
+## TODO
+Include some demo code.
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+/*
+ * Build project assets for development and production
+ *
+ * Installation (Node Package Manager):
+ * > npm install --global gulp bower
+ * > npm install && bower install
+ *
+ * Usage (GulpJS):
+ * > gulp styles  [--production][--src=<filepath/filename.scss> [--dest=<path/dirname>]]
+ * > gulp scripts [--production][--src=<filepath/filename.js> [--dest=<path/dirname>]]
+ */
+'use strict';
+var // defaults
+    defassets_srcdir = "assets/",
+    defvendor_srcdir = defassets_srcdir+"vendor/",
+    defstyles_srcdir = defassets_srcdir+"scss/",
+    defstyles_srcglb = defstyles_srcdir+"*.scss",
+    defscripts_srcglb = defassets_srcdir+"es6/*.js",
+    defassets_destdir = defassets_srcdir,
+    defstyles_destdir = defassets_destdir+"css/",
+    defscripts_destdir = defassets_destdir+"js/",
+    // global modules
+    args = require('yargs').argv,
+    gulp = require('gulp'),
+    gulpif = require('gulp-if'),
+    rename = require('gulp-rename');
+
+gulp.task('default', ['styles', 'scripts']);
+
+gulp.task('styles', function(){
+var sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    srcfiles = args.src || defstyles_srcglb,
+    destdir = args.dest || defstyles_destdir;
+    return gulp.src( srcfiles )
+    .pipe( sourcemaps.init() )
+    .pipe( sass( {
+        errLogToConsole: true,
+        outputStyle: 'nested',
+        //sourceComments: true,
+        sourceMapEmbed: false,
+        includePaths: [
+          defvendor_srcdir+"foundation-sites/scss",
+          defvendor_srcdir+"font-awesome/scss",
+          defvendor_srcdir+"motion-ui/src"
+        ] } ) )
+    .pipe( autoprefixer( {
+        cascade: false,
+        //map: true,
+        browsers: ["last 2 versions", "iOS >= 7"] } ) )
+    .pipe( sourcemaps.write( "./", {
+        includeContent: false,
+        sourceRoot: "../scss" } ) )
+    .pipe( gulp.dest( destdir ) )
+    .pipe( gulpif(args.production, rename( { suffix: ".min" } ) ) )
+    .pipe( gulpif(args.production, minifycss() ) )
+    .pipe( gulpif(args.production, gulp.dest( destdir ) ) )
+});
+
+gulp.task('scripts', function(){
+var include = require('gulp-include'), // extend Javascript files with Sprockets syntax
+    uglify = require('gulp-uglify'),
+    srcfiles = args.src || defscripts_srcglb,
+    destdir = args.dest || defscripts_destdir;
+    return gulp.src( srcfiles )
+    .pipe( include() )
+    .pipe( gulp.dest( destdir ) )
+    .pipe( gulpif(args.production, rename( function(fullname){ fullname.extname = ".min.js"; } ) ) )
+    .pipe( gulpif(args.production, uglify( { preserveComments: "license" } ) ) )
+    .pipe( gulpif(args.production, gulp.dest( destdir ) ) )
+});
+
+gulp.task('watch', function(){
+    gulp.watch( defstyles_srcglb, ['styles'] );
+    gulp.watch( defscripts_srcglb, ['scripts'] );
+});
