7dd0c52038dd633ba8256edfaca307a1fee19719
zietbukuel <sietbukuel@gmail.com>
2016-07-18 04:26:11 -0500

    - Using gulp-clean-css instead of gulp-minify-css. - Removing .bowerrc and using default path (bower_components) because installing from the backend does not include this file and compilation of assets will fail. - Using pump in gulpfile.js for better error handling/display. - Now the JS compilation uses Babel (es2015 preset) to avoid Unexpected character errors. - New "gulp clean" command to delete old CSS and JS files. This command will delete the entire assets/js and assets/css folders.

--- assets/es6/app.js
+++ assets/es6/app.js
@@ -1,44 +1,40 @@
-//= require ../vendor/jquery/dist/jquery.js
-//= require ../vendor/what-input/what-input.js
-//= require ../vendor/motion-ui/motion-ui.js
-//= require ../vendor/foundation-sites/js/foundation.core.js
-//= require ../vendor/foundation-sites/js/foundation.abide.js
-//= require ../vendor/foundation-sites/js/foundation.accordion.js
-//= require ../vendor/foundation-sites/js/foundation.accordionMenu.js
-//= require ../vendor/foundation-sites/js/foundation.drilldown.js
-//= require ../vendor/foundation-sites/js/foundation.dropdown.js
-//= require ../vendor/foundation-sites/js/foundation.dropdownMenu.js
-//= require ../vendor/foundation-sites/js/foundation.equalizer.js
-//= require ../vendor/foundation-sites/js/foundation.interchange.js
-//= require ../vendor/foundation-sites/js/foundation.magellan.js
-//= require ../vendor/foundation-sites/js/foundation.offcanvas.js
-//= require ../vendor/foundation-sites/js/foundation.orbit.js
-//= require ../vendor/foundation-sites/js/foundation.responsiveMenu.js
-//= require ../vendor/foundation-sites/js/foundation.responsiveToggle.js
-//= require ../vendor/foundation-sites/js/foundation.reveal.js
-//= require ../vendor/foundation-sites/js/foundation.slider.js
-//= require ../vendor/foundation-sites/js/foundation.sticky.js
-//= require ../vendor/foundation-sites/js/foundation.tabs.js
-//= require ../vendor/foundation-sites/js/foundation.toggler.js
-//= require ../vendor/foundation-sites/js/foundation.tooltip.js
-//= require ../vendor/foundation-sites/js/foundation.util.box.js
-//= require ../vendor/foundation-sites/js/foundation.util.keyboard.js
-//= require ../vendor/foundation-sites/js/foundation.util.mediaQuery.js
-//= require ../vendor/foundation-sites/js/foundation.util.motion.js
-//= require ../vendor/foundation-sites/js/foundation.util.nest.js
-//= require ../vendor/foundation-sites/js/foundation.util.timerAndImageLoader.js
-//= require ../vendor/foundation-sites/js/foundation.util.touch.js
-//= require ../vendor/foundation-sites/js/foundation.util.triggers.js
+//= require ../../bower_components/jquery/dist/jquery.js
+//= require ../../bower_components/what-input/what-input.js
+//= require ../../bower_components/motion-ui/motion-ui.js
+//= require ../../bower_components/foundation-sites/js/foundation.core.js
+//= require ../../bower_components/foundation-sites/js/foundation.abide.js
+//= require ../../bower_components/foundation-sites/js/foundation.accordion.js
+//= require ../../bower_components/foundation-sites/js/foundation.accordionMenu.js
+//= require ../../bower_components/foundation-sites/js/foundation.drilldown.js
+//= require ../../bower_components/foundation-sites/js/foundation.dropdown.js
+//= require ../../bower_components/foundation-sites/js/foundation.dropdownMenu.js
+//= require ../../bower_components/foundation-sites/js/foundation.equalizer.js
+//= require ../../bower_components/foundation-sites/js/foundation.interchange.js
+//= require ../../bower_components/foundation-sites/js/foundation.magellan.js
+//= require ../../bower_components/foundation-sites/js/foundation.offcanvas.js
+//= require ../../bower_components/foundation-sites/js/foundation.orbit.js
+//= require ../../bower_components/foundation-sites/js/foundation.responsiveMenu.js
+//= require ../../bower_components/foundation-sites/js/foundation.responsiveToggle.js
+//= require ../../bower_components/foundation-sites/js/foundation.reveal.js
+//= require ../../bower_components/foundation-sites/js/foundation.slider.js
+//= require ../../bower_components/foundation-sites/js/foundation.sticky.js
+//= require ../../bower_components/foundation-sites/js/foundation.tabs.js
+//= require ../../bower_components/foundation-sites/js/foundation.toggler.js
+//= require ../../bower_components/foundation-sites/js/foundation.tooltip.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.box.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.keyboard.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.mediaQuery.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.motion.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.nest.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.timerAndImageLoader.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.touch.js
+//= require ../../bower_components/foundation-sites/js/foundation.util.triggers.js
 
 jQuery(document).ready(function($){
-
-  $(document).foundation();
-
+  $(document).foundation(); // Initialize Foundation
 });
 
-
 /* Theme Javascript */
-
 jQuery(document).ready(function($){
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -12,67 +12,89 @@
 'use strict';
 var // defaults
     defassets_srcdir = "assets/",
-    defvendor_srcdir = defassets_srcdir+"vendor/",
+    defvendor_srcdir = "bower_components/",
     defstyles_srcdir = defassets_srcdir+"scss/",
     defstyles_srcglb = defstyles_srcdir+"*.scss",
     defscripts_srcglb = defassets_srcdir+"es6/*.js",
     defassets_destdir = defassets_srcdir,
     defstyles_destdir = defassets_destdir+"css/",
-    defscripts_destdir = defassets_destdir+"js/",
-    // global modules
+    defscripts_destdir = defassets_destdir+"js/";
+    
+var // global modules
     args = require('yargs').argv,
     gulp = require('gulp'),
     gulpif = require('gulp-if'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    pump = require('pump');
 
 gulp.task('default', ['styles', 'scripts']);
 
-gulp.task('styles', function(){
-var sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    srcfiles = args.src || defstyles_srcglb,
-    destdir = args.dest || defstyles_destdir;
-    return gulp.src( srcfiles )
-    .pipe( sourcemaps.init() )
-    .pipe( sass( {
-        errLogToConsole: true,
-        outputStyle: 'nested',
-        //sourceComments: true,
-        sourceMapEmbed: false,
-        includePaths: [
-          defvendor_srcdir+"foundation-sites/scss",
-          defvendor_srcdir+"font-awesome/scss",
-          defvendor_srcdir+"motion-ui/src"
-        ] } ) )
-    .pipe( autoprefixer( {
-        cascade: false,
-        //map: true,
-        browsers: ["last 2 versions", "iOS >= 7"] } ) )
-    .pipe( sourcemaps.write( "./", {
-        includeContent: false,
-        sourceRoot: "../scss" } ) )
-    .pipe( gulp.dest( destdir ) )
-    .pipe( gulpif(args.production, rename( { suffix: ".min" } ) ) )
-    .pipe( gulpif(args.production, minifycss() ) )
-    .pipe( gulpif(args.production, gulp.dest( destdir ) ) )
+gulp.task('styles', function(cb) {
+    var sourcemaps = require('gulp-sourcemaps'),
+        sass = require('gulp-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        cleancss = require('gulp-clean-css'),
+        srcfiles = args.src || defstyles_srcglb,
+        destdir = args.dest || defstyles_destdir;
+
+    pump([
+        gulp.src( srcfiles ),
+        sourcemaps.init(),
+        sass( {
+            errLogToConsole: true,
+            outputStyle: 'nested',
+            //sourceComments: true,
+            sourceMapEmbed: false,
+            includePaths: [
+            defvendor_srcdir+"foundation-sites/scss",
+            defvendor_srcdir+"font-awesome/scss",
+            defvendor_srcdir+"motion-ui/src"
+            ]
+        } ),
+        autoprefixer( {
+            cascade: false,
+            //map: true,
+            browsers: ["last 2 versions", "iOS >= 7"]
+        } ),
+        sourcemaps.write( "./", {
+            includeContent: false,
+            sourceRoot: "../scss" 
+        } ),
+        gulp.dest( destdir ),
+        gulpif(args.production, rename( { suffix: ".min" } ) ),
+        gulpif(args.production, cleancss() ),
+        gulpif(args.production, gulp.dest( destdir ) )
+    ], cb);
 });
 
-gulp.task('scripts', function(){
-var include = require('gulp-include'), // extend Javascript files with Sprockets syntax
-    uglify = require('gulp-uglify'),
-    srcfiles = args.src || defscripts_srcglb,
-    destdir = args.dest || defscripts_destdir;
-    return gulp.src( srcfiles )
-    .pipe( include() )
-    .pipe( gulp.dest( destdir ) )
-    .pipe( gulpif(args.production, rename( function(fullname){ fullname.extname = ".min.js"; } ) ) )
-    .pipe( gulpif(args.production, uglify( { preserveComments: "license" } ) ) )
-    .pipe( gulpif(args.production, gulp.dest( destdir ) ) )
+gulp.task('scripts', function(cb) {
+    var include = require('gulp-include'), // extend Javascript files with Sprockets syntax
+        babel = require('gulp-babel'),
+        uglify = require('gulp-uglify'),
+        srcfiles = args.src || defscripts_srcglb,
+        destdir = args.dest || defscripts_destdir;
+
+    pump([
+        gulp.src( srcfiles ),
+        include(),
+        gulp.dest( destdir ),
+        gulpif(args.production, rename( function(fullname){ fullname.extname = ".min.js"; } ) ),
+        gulpif(args.production, babel( { presets: ['es2015'] } ) ),
+        gulpif(args.production, uglify( { preserveComments: "license" } ) ),
+        gulpif(args.production, gulp.dest( destdir ) )
+    ], cb);
 });
 
-gulp.task('watch', function(){
+gulp.task('clean', function() {
+    var del = require('del');
+    
+    return del([
+        'assets/css',
+        'assets/js'
+    ]);
+});
+
+gulp.task('watch', function() {
     gulp.watch( [defstyles_srcglb], ['styles'] );
     gulp.watch( [defscripts_srcglb], ['scripts'] );
 });
